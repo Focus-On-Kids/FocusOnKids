6463760b6c029b9427f772272c672d7d0133bede
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-05-04 20:43:02 -0300

    creacion de componente layout como contenedor general, y agregado de estilos de normalizació

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import style from './layout.module.css';
+
+const Layout = () => {
+  return (
+    <body className={style.flexWrapper}>
+      {/* <Header/> */}
+
+      {/* contenido  */}
+
+      {/* <Footer/> */}
+    </body>
+  );
+};
+
+export default Layout;
