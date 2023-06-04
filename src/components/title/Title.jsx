bc274ae9842243cf943dbabb925215df84362be4
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-06-04 16:27:00 -0300

    correcion de componente title, y posisionamiento de imagenes de banderas

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,9 +1,19 @@
 import styles from './title.module.css';
+import icons from '../../assets/icons/index';
 
-export function Title({ children }) {
+export function Title({ children, flags = false }) {
   return (
-    <div className={styles.title}>
-      <h1 className={styles.title__h1}>{children}</h1>
-    </div>
+    <>
+      <div className={styles.title}>
+        <h1 className={styles.title__h1}>{children}</h1>
+      </div>
+      {flags && (
+        <div className={styles.flag}>
+          <img src={icons.es_flag} alt="Spanish language" />
+          <img src={icons.it_flag} alt="Italian language" />
+          {flags}
+        </div>
+      )}
+    </>
   );
 }
